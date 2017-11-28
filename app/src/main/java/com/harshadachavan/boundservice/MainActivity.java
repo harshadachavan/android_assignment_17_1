537f3cc11f0f107f24a5aa3bc5c3d321c9d9027e
Harshada <harshada@destaglobal.com>
2017-11-28 18:02:14 +0530

    Services and pending intent

--- app/src/main/java/com/harshadachavan/boundservice/MainActivity.java
+++ app/src/main/java/com/harshadachavan/boundservice/MainActivity.java
@@ -3,6 +3,7 @@ package com.harshadachavan.boundservice;
 import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -41,5 +42,9 @@ public class MainActivity extends Activity {
     public void stopAudio(View view) {
         Intent serviceIntent = new Intent(this, UnboundMyService.class);
         stopService(serviceIntent);
+
+        String ns = Context.NOTIFICATION_SERVICE;
+        NotificationManager nMgr = (NotificationManager) getApplicationContext().getSystemService(ns);
+        nMgr.cancel(1);
     }
 }
